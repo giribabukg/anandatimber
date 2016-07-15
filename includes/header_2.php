af991572ba9af575b353678a217f16dc39227217
giribabukg2 <giribabukg2@6a8be499-bde3-4135-bfb0-0457c0fbf94e>
2016-07-15 10:40:03 +0000

    LIve code committed

---
+++ includes/header_2.php
@@ -0,0 +1,13 @@
+  <div class="header">
+    <div class="page-wrap">
+      <div class="header-left"><img src="images/atb.jpg" alt="Anandha Timber Depot" title="Anandha Timber Depot" /></div>
+      <div class="header-right"> <i>For Support</i><br />
+        <b>Call:</b> 98400 23881 </div>
+      <div class="clear"></div>
+	  <?php include_once('includes/main_menu.php'); ?>
+      <div class="clear"></div>
+	  <?php include_once('includes/main_banner.php'); ?>
+      <!-- // Menu -->
+      <div class="clear"></div>
+    </div>
+  </div>
