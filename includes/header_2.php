d5e99db677940f018e4379ad8e1874272c66763f
giribabukg2 <giribabukg2@6a8be499-bde3-4135-bfb0-0457c0fbf94e>
2016-10-22 00:49:52 +0000

    Banner for foreign site done.

--- includes/header_2.php
+++ includes/header_2.php
@@ -1,6 +1,6 @@
   <div class="header">
     <div class="page-wrap">
-      <div class="header-left"><img src="images/atb2.jpg" alt="Anandha Timber Depot" title="Anandha Timber Depot" /><div class="mn-logo">Ananda Timber Depot</div></div>
+      <div class="header-left"><img src="images/ananda-logo3.png" alt="Anandha Timber Depot" title="Anandha Timber Depot" /><div class="mn-logo">Ananda Timber Depot</div></div>
       <div class="header-right"> <i>For Support</i><br />
         <b>Call:</b> 98400 23881 </div>
       <div class="clear"></div>
